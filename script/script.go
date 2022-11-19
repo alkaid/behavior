35df0ceaa865476416508c37f7b73794bf9a71dd
alkaid <xxxxxxxxx>
2022-11-19 22:20:43 +0800

    build: 由于cicd暂不支持glibc,而gengine使用了大量c库方法，先屏蔽

--- script/script.go
+++ script/script.go
@@ -3,44 +3,46 @@ package script
 import (
 	"fmt"
 
-	"github.com/alkaid/behavior/logger"
-	"github.com/bilibili/gengine/engine"
 	"github.com/pkg/errors"
+
+	"github.com/alkaid/behavior/logger"
 	"go.uber.org/zap"
 )
 
-var pool *engine.GenginePool    // 引擎池单例
+// var pool *engine.GenginePool    // 引擎池单例
 var codes = map[string]string{} // 代码,索引为nodeID
 var apiLib = map[string]any{}   // 公共api库
 
 // InitPool 初始化规则引擎池
-//  @param poolMinLen 最小容量
-//  @param poolMaxLen 最大容量
-//  @param api 注入的API
-//  @return error
+//
+//	@param poolMinLen 最小容量
+//	@param poolMaxLen 最大容量
+//	@param api 注入的API
+//	@return error
 func InitPool(poolMinLen, poolMaxLen int, api map[string]any) error {
-	if pool != nil {
-		logger.Log.Warn("init goroutine pool duplicate")
-		return nil
-	}
-	RegisterApi(api)
-	var err error
-	// 实例化pool时脚本不能为空
-	codeWrap := fmt.Sprintf(`
-rule "%s"
-begin
-	%s
-end
-	`, "donothing", "")
-	pool, err = engine.NewGenginePool(int64(poolMinLen), int64(poolMaxLen), engine.SortModel, codeWrap, apiLib)
-	if err != nil {
-		return errors.WithStack(err)
-	}
+	// 	if pool != nil {
+	// 		logger.Log.Warn("init goroutine pool duplicate")
+	// 		return nil
+	// 	}
+	// 	RegisterApi(api)
+	// 	var err error
+	// 	// 实例化pool时脚本不能为空
+	// 	codeWrap := fmt.Sprintf(`
+	// rule "%s"
+	// begin
+	// 	%s
+	// end
+	// 	`, "donothing", "")
+	// 	pool, err = engine.NewGenginePool(int64(poolMinLen), int64(poolMaxLen), engine.SortModel, codeWrap, apiLib)
+	// 	if err != nil {
+	// 		return errors.WithStack(err)
+	// 	}
 	return nil
 }
 
 // RegisterApi 向引擎池注册公共API
-//  @param api
+//
+//	@param api
 func RegisterApi(api map[string]any) {
 	for name, f := range api {
 		_, ok := apiLib[name]
@@ -53,8 +55,9 @@ func RegisterApi(api map[string]any) {
 }
 
 // RegisterCode 注册代码
-//  @param name 规则名称 一般是nodeID
-//  @param code 代码
+//
+//	@param name 规则名称 一般是nodeID
+//	@param code 代码
 func RegisterCode(name string, code string) {
 	codeWrap := fmt.Sprintf(`
 rule "%s"
@@ -66,29 +69,34 @@ end
 }
 
 // UpdateCode 全量更新池子里的代码
-//  @return error
+//
+//	@return error
 func UpdateCode() error {
-	all := ""
-	for _, code := range codes {
-		all += code
-	}
-	err := pool.UpdatePooledRules(all)
-	if err != nil {
-		return errors.WithStack(err)
-	}
+	// all := ""
+	// for _, code := range codes {
+	// 	all += code
+	// }
+	// err := pool.UpdatePooledRules(all)
+	// if err != nil {
+	// 	return errors.WithStack(err)
+	// }
 	return nil
 }
 func ExistsCode(name string) bool {
-	return pool.IsExist([]string{name})[0]
+	// return pool.IsExist([]string{name})[0]
+	return false
 }
 
 // RunCode 执行代码
-//  @param name
-//  @param env
+//
+//	@param name
+//	@param env
 func RunCode(name string, env map[string]any) (out any, err error) {
-	err, rets := pool.ExecuteSelectedRules(env, []string{name})
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	return rets[name], nil
+	// err, rets := pool.ExecuteSelectedRules(env, []string{name})
+	// if err != nil {
+	// 	return nil, errors.WithStack(err)
+	// }
+	// return rets[name], nil
+	err = errors.New("UnSupport because not compile with glibc now...")
+	return nil, err
 }
